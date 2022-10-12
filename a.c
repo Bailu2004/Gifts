7075552c3796de2a57d34df719c922f805bcbe93
Bailu2004 <115567869+Bailu2004@users.noreply.github.com>
2022-10-12 20:13:49 +0800

    Add files via upload

---
+++ a.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int sum(int a[ ], unsigned len) {
+    int i, sum = 0;
+    for (i = 0; i <= len-1; i++)
+        sum += a[i];
+    return sum;
+}
+
+int main(){
+	printf("hello~");
+}
